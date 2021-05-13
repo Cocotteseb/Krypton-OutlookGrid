e498e9018c8822496ef5ff55dec27c579c1fa827
Cocotteseb <cocotteseb@jdhsoftware.com>
2021-05-13 17:39:38 +0200

    header updates

--- Source/KryptonOutlookGrid/KryptonOutlookGrid/OutlookGrid.cs
+++ Source/KryptonOutlookGrid/KryptonOutlookGrid/OutlookGrid.cs
@@ -9,12 +9,12 @@
 //-----------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------
-// Copyright (C) 2013-2015 JDH Software - <support@jdhsoftware.com>
+// Copyright (C) 2013-2021 JDH Software - <support@jdhsoftware.com>
 //
 // This program is provided to you under the terms of the Microsoft Public
-// License (Ms-PL) as published at https://kryptonoutlookgrid.codeplex.com/license
+// License (Ms-PL) as published at https://github.com/Cocotteseb/Krypton-OutlookGrid/blob/master/LICENSE.md
 //
-// Visit http://www.jdhsoftware.com and follow @jdhsoftware on Twitter
+// Visit https://www.jdhsoftware.com and follow @jdhsoftware on Twitter
 //
 //--------------------------------------------------------------------------------
 using ComponentFactory.Krypton.Toolkit;
